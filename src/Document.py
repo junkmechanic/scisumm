b4b66a9a0a05772fc47742e83d7b7d5054b4816e
ankur.junkmechanic <ankurcool.unforgiven@gmail.com>
2013-10-22 05:29:11 +0800

    Nothing much

--- src/Document.py
+++ src/Document.py
@@ -1,14 +1,32 @@
 from lxml import etree
-import nltk
+from nltk.tokenize import sent_tokenize
 
 
 class Document:
 
     def __init__(self, xmlfile):
+        self.document = {}
         tree = etree.parse(xmlfile)
         algrthms = tree.getroot()
-        for section in algrthms.iterdescendants(['sectionHeader', 'bodyText']):
-            print(section.text)
+        block = algrthms.iterdescendants(['sectionHeader', 'bodyText'])
+        try:
+            section = ''
+            while True:
+                blk = block.next()
+                if(blk.tag == 'sectionHeader'):
+                    self.document[blk.text] = sent_tokenize(
+                        block.next().text)
+                    section = blk.text
+                else:
+                    self.document[section] = self.document[section] + \
+                        sent_tokenize(blk.text)
+        except StopIteration:
+            pass
+        except Exception:
+            print "Something went wrong!"
+        for key, val in self.document.iteritems():
+            print key
+            print val
 
 
 if __name__ == '__main__':
