30e5976242f8995ed40b606e62510641dd4a29ff
junkmechanic <ankurcool.unforgiven@gmail.com>
2014-02-25 12:51:05 +0800

    With test/train data

--- src/explore.py
+++ src/explore.py
@@ -34,3 +34,21 @@ print "Positive Samples Classified Correctly : " + str(pos['right'])
 print "Positive Samples Classified Incorrectly : " + str(pos['wrong'])
 print "Negative Samples Classified Correctly : " + str(neg['right'])
 print "Negative Samples Classified Incorrectly : " + str(neg['wrong'])
+
+
+def display(real=1, pred=1):
+    for key in data.keys():
+        if int(data[key]['reallbl']) == real:
+            prediction = -1 if data[key]['svmval'] < 0 else +1
+            if prediction == pred:
+                yield data[key]
+
+
+def list_sentences(real=1, pred=1):
+    for sent in display(real, pred):
+        print sent['contextpre']
+        print "---Main sentence start-----"
+        print sent['sentence']
+        print "---Main sentence end-----"
+        print sent['contextpos']
+        raw_input()
